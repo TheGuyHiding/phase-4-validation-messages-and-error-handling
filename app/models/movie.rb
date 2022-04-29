8cfba4135d7096b754430373969ebc75f56b9914
Luis Ibarra <luis9609@gmail.com>
2022-04-29 11:59:47 -0600

    commit tree d0a0eb94b0a4681f6c5f645293bfc5d693095c8d

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ApplicationRecord
+    CATEGORIES = ['Comedy', "Drama", "Animation", "Mystery", "Horror", "Fantasy"]
+
+    validates :title, presence: true
+    validates :year, numericality: {
+        greater_than_or_equal_to: 1888,
+        less_than_or_equal_to: Date.today.year
+    }
+    validates :poster_urlm presence: true
+    validates :category, inclusion: {
+        in: CATEGORIES,
+        message: "must be one of: #{CATEGORIES.join(', ')}" 
+    }
+
 end
